dd4bd13bbc6d194a8f095991bb40f21aaa35d189
Igor Freitas <igorddf@gmail.com>
2018-12-11 16:24:34 -0200

    insercao da hora no assunto do email

--- Utils_tracking.py
+++ Utils_tracking.py
@@ -64,7 +64,7 @@ def sendMailAlert(sender, recipients, frame, tipoObjetoDetectado):
 
 
     msg = MIMEMultipart()
-    msg['Subject'] = 'PV - ' + '"' + tipoObjetoDetectado + '"' + ' detectado'
+    msg['Subject'] = 'PV - ' + '"' + tipoObjetoDetectado + '"' + ' detectado' + data['hour']'
     msg['From'] = sender
     msg['To'] = recipients
     data = getDate()
@@ -95,6 +95,32 @@ def sendMailAlert(sender, recipients, frame, tipoObjetoDetectado):
 
 #sendMailAlert('igorddf@gmail.com', 'igorddf@gmail.com', 'foto_alarme.png')
 
+#def isMotionDetected(width, height, frame):
+#    cv.CvtColor(frame, self.frame2gray, cv.CV_RGB2GRAY)
+#
+#    #Absdiff to get the difference between to the frames
+#    cv.AbsDiff(self.frame1gray, self.frame2gray, self.res)
+#
+#    #Remove the noise and do the threshold
+#    cv.Smooth(self.res, self.res, cv.CV_BLUR, 5,5)
+#    element = cv.CreateStructuringElementEx(5*2+1, 5*2+1, 5, 5,  cv.CV_SHAPE_RECT)
+#    cv.MorphologyEx(self.res, self.res, None, None, cv.CV_MOP_OPEN)
+#    cv.MorphologyEx(self.res, self.res, None, None, cv.CV_MOP_CLOSE)
+#    cv.Threshold(self.res, self.res, 10, 255, cv.CV_THRESH_BINARY_INV)
+#
+#
+#    nb=0 #Will hold the number of black pixels
+#
+#    for y in range(height): #Iterate the hole image
+#        for x in range(width):
+#            if self.res[y,x] == 0.0: #If the pixel is black keep it
+#                nb += 1
+#    avg = (nb*100.0)/self.nb_pixels #Calculate the average of black pixel in the image
+#    #print "Average: ",avg, "%\r",
+#    if avg > self.ceil:#If over the ceil trigger the alarm
+#        return True
+#    else:
+#        return False
 
 
 
