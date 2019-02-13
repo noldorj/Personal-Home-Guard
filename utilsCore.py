ec838214c9e2c2fd85eec6478cf83cd85f81f2aa
igorj <igorddf@gmail.com>
2019-02-13 17:49:47 -0200

    json read, add, delete

--- utilsCore.py
+++ utilsCore.py
@@ -114,20 +114,62 @@ class StatusConfig:
         self.data["emailConfig"]["subject"]   = emailConfig["subject"]
         self.data["emailConfig"]["to"]        = emailConfig["to"]
 
+    def addRegion(self, nameRegion, isEmailAlert, isSoundAlert, time, days, objectType, prob_threshold, pointsPolygon):
+
+        self.region["nameRegion"]               = nameRegion
+        self.region["isEmailAlert"]             = isEmailAlert
+        self.region["isSoundAlert"]             = isSoundAlert
+        self.region["time"]["start"]            = time["start"]
+        self.region["time"]["end"]              = time["end"]
+        self.region["days"]["mon"]              = days["mon"]
+        self.region["days"]["tues"]             = days["tues"]
+        self.region["days"]["wed"]              = days["wed"]
+        self.region["days"]["thurs"]            = days["thurs"]
+        self.region["days"]["fri"]              = days["fri"]
+        self.region["days"]["sat"]              = days["sat"]
+        self.region["days"]["sun"]              = days["sun"]
+        self.region["objectType"]["person"]     = objectType["person"]
+        self.region["objectType"]["car"]        = objectType["car"]
+        self.region["objectType"]["bike"]       = objectType["bike"]
+        self.region["objectType"]["dog"]        = objectType["dog"]
+        self.region["prob_threshold"]           = prob_threshold
+        self.region["pointsPolygon"]            = pointsPolygon
+
+        self.regions["regions"].append(self.region)
+        
 
 #    def __init__(self,isRecording, isEmailAlert, isGateSelected, isSoundAlert,
 #                 dnnModel, emailConfig, dirVideos):
 
-    def __init__(self, configFile):
+    def __init__(self, configFile='config.json', regionsFile='regions.json'):
         #configuracoes setadas pelo arquivo sobrescrevem as configuracoes padroes
         self.readConfigFile(configFile)
+        self.readRegionsFile(regionsFile)
 
     def readConfigFile(self, file = 'config.json'):
-
         print('Lendo arquivo de configuração: ' + os.getcwd() + '/' + file)
         self.data = json.load(open(file,'r'))
         self.printConfig()
 
+    def readRegionsFile(self, file = 'regions.json'):
+        print('Lendo arquivo de regiões: ' + os.getcwd() + '/' + file)
+        self.regions = json.load(open(file,'r'))
+        self.printRegions()
+    
+    def deleteRegion(self, nameRegion='regions1'):
+        i = 0
+        print('Region {} to be removed'.format(nameRegion))
+        
+        for r in self.regions["regions"]:
+            print('nameRegion {}'.format(r.get("nameRegion")))
+            if r.get("nameRegion") == nameRegion:                
+                del self.regions["regions"][i]
+                print('Region {} removed i: {}'.format(nameRegion, i))
+                return True            
+            
+            i = i+1
+        return False
+        
 
 
     def printConfig(self):
@@ -147,11 +189,38 @@ class StatusConfig:
         print('emailConfig/smtp:  {}'.format(self.data.get('emailConfig').get('smtp')))
         print('emailConfig/port:  {}'.format(self.data.get('emailConfig').get('port')))
         print('dirVideos:         {}'.format(self.data.get('dirVideos')))
+        
+        
+    def printRegions(self):
+            print('                                      ')
+            print('--- Regions status  ---')
+            print('# regions: {}'.format(len(self.regions["regions"])))
+            for r in self.regions["regions"]:
+                
+                print('nameRegion:            {}'.format(r.get('nameRegion')))
+                print('isEmailAlert:          {}'.format(r.get('isEmailAlert')))
+                print('isSoundAlert:          {}'.format(r.get('isSoundAlert')))
+                print('Prob_threshold:        {}'.format(r.get('prob_threshold')))   
+                print('Alarm Time start:      {}'.format(r.get('time').get('start')))
+                print('Alarm Time end:        {}'.format(r.get('time').get('end')))
+                print('PointsPolygon:         {}'.format(r.get('pointsPolygon')))   
+                print('Alarm Days:')
+                print('  Monday:        {}'.format(r.get('days').get('mon')))                
+                print('  Tuesday:       {}'.format(r.get('days').get('tues')))                
+                print('  Wednesday:     {}'.format(r.get('days').get('wed')))                
+                print('  Thrusday:      {}'.format(r.get('days').get('thurs')))                
+                print('  Friday:        {}'.format(r.get('days').get('fri')))                
+                print('  Saturday:      {}'.format(r.get('days').get('sat')))                
+                print('  Sunday:        {}'.format(r.get('days').get('sun')))                                   
+                print('----------------------------------------------------------------')
 
-
+    def saveRegionFile(self, file = 'region.json'):
+        print('Salvando arquivo de regiones: ' + os.getcwd() + '/' + file)
+        json.dump(self.regions, open(file, 'w'), indent=4)
+    
+    
     def saveConfigFile(self, file = 'config.json'):
-
-        # print('Salvando arquivo de configuração: ' + os.getcwd() + '/' + file)
+        print('Salvando arquivo de configuração: ' + os.getcwd() + '/' + file)
         json.dump(self.data, open(file,'w'), indent=4)
 
 
@@ -162,3 +231,25 @@ def playSound():
     pygame.init()
     pygame.mixer.music.load('campainha.mp3')
     pygame.mixer.music.play(0)
+    
+
+status = StatusConfig()
+
+t = {"start":"7:00", "end":"12:00"}
+days = {'mon':'False', 'tues':'False','wed':'False', 'thurs':'False', 'fri':'False','sat':'False','sun':'False'}
+objectType = {'person':'teste', 'car':'teste', 'bike':'teste', 'dog':'teste'}
+points = [[[15,15],[15,65],[65,15],[65,65]]]
+
+status.addRegion("teste", "true", "true", t, days, objectType, 0.45, points)
+status.addRegion("teste2", "true2", "true2", t, days, objectType, 0.45, points)
+
+status.printRegions()
+status.deleteRegion(nameRegion="teste")
+
+status.saveRegionFile('region.teste.json')
+status2 = StatusConfig(regionsFile='region.teste.json')
+status.regions
+status.regions["regioes"][1]
+
+
+
