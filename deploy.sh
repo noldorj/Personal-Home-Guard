d002e1e5225c777768450357c3440e6cca10b25d
igor <igorddf@gmail.com>
2020-03-05 10:45:26 -0300

    gera executavel na pasta ../deploy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
-pyinstaller portao_virtual_tracking.py -F -y -p /home/igor/openvino:. --hidden-import numpy --hidden-import scipy --hidden-import scipy._lib.messagestream --add-data /home/igor/openvino:. --add-data /home/igor/openvino/deployment_tools/inference_engine/lib/intel64:. --add-binary /opt/intel/openvino/opencv/lib:. --add-binary /home/igor/openvino/deployment_tools/inference_engine/lib/intel64:. --hidden-import opencv -p /opt/intel/openvino/opencv/:.
+echo "copiando arquivos..."
+cp portao_virtual_tracking.py pluginOpenVino.py mainForm.py utilsCore.py Utils_tracking.py -r objectTracking ../deploy/
+cp campainha.mp3 config.json regions.json -r dlModels ../deploy
+cp -r openvino ../deploy
 
-cp campainha.mp3 regions.json regions.json -r dlModels ../deploy
+echo "gerando executavel..."
+
+pyinstaller portao_virtual_tracking.py -F -y -p openvino:. --hidden-import numpy --hidden-import scipy --hidden-import scipy._lib.messagestream --add-data openvino:. --add-data openvino/deployment_tools/inference_engine/lib/intel64:. --add-data /home/igor/Dropbox/deploy/regions.json:. --add-data /home/igor/Dropbox/deploy/config.json:. --add-data /home/igor/Dropbox/deploy/campainha.mp3:. --add-data /home/igor/Dropbox/deploy/dlModels:. --add-binary openvino/opencv/lib:. --add-binary openvino/deployment_tools/inference_engine/lib/intel64:. --hidden-import opencv -p openvino/opencv:. --distpath ../deploy/
+
+echo "executavel done"
+
+echo "apagando arquivos"
+rm *.py
+rm -rf objectTracking
