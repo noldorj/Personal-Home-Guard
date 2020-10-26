0b24c66e9d6ab0a4cc9a9125f927a61121f22ef1
Igor Freitas <igorddf@gmail.com>
2020-10-26 13:15:28 -0300

    deploy.bat ok - arquivo config.json apontando para modelo openvino antigo .xml e .bin - instrucoes para matplotlib no windows ok

--- deploy.bat
+++ deploy.bat
@@ -1,12 +1,11 @@
 echo "copiando arquivos..."
 
-xcopy "campainha.mp3" config.json regions.json -r dlModels ../deploy-win
-xcopy -r openvino_deploy_package_win_2020-4 ../deploy-win/openvino
+#xcopy "campainha.mp3" config.json regions.json -r dlModels ../deploy-win
+#xcopy -r openvino_deploy_package_win_2020-4 ../deploy-win/openvino
 
 echo "gerando executavel..."
 
-pyinstaller portao_virtual_tracking.py -y --onefile --distpath ..\deploy-win --add-data openvino_deploy_package_win_2020-4\deployment_tools\inference_engine\bin\intel64\Release\plugins.xml;. --noconfirm --hidden-import matplotlib
+pyinstaller portao_virtual_tracking.py --onefile -y --add-data "regions.json;." --add-data "config.json;." --add-data "campainha.mp3;." --add-data "dlModels;." --distpath ../deploy-win  --add-binary "C:\Program Files (x86)\Intel\openvino_2019.1.148\inference_engine\bin\intel64\Release;." --add-data "C:\Program Files (x86)\Intel\openvino_2019.1.148\inference_engine\bin\intel64\Release;." -p "C:\Program Files (x86)\Intel\openvino_2019.1.148\opencv;." --add-binary "C:\Program Files (x86)\Intel\openvino_2019.1.148\opencv\lib;." --hidden-import opencv --noconfirm --hidden-import matplotlib -p "C:\Users\pv\AppData\Local\Programs\Python\Python37\Lib\site-packages\matplotlib;."
 
 echo "executavel done"
-
 
